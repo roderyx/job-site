2b22dbaa9f77ef7d067f99cb1e93d7dc27eb928f
roderyx <roderyj@gmail.com>
2025-08-28 19:45:51 -0600

    added avatar

--- job-site/src/app/features/users/components/_SidebarUserButtonClient.tsx
+++ job-site/src/app/features/users/components/_SidebarUserButtonClient.tsx
@@ -1,7 +1,13 @@
 "use client";
-import { SidebarMenu } from "@/components/ui/sidebar";
+import { SidebarMenu, SidebarMenuButton } from "@/components/ui/sidebar";
 import { useIsMobile } from "@/hooks/use-mobile";
-import { DropdownMenu } from "@radix-ui/react-dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@radix-ui/react-dropdown-menu";
+import { ChevronsUpDown } from "lucide-react";
 import React from "react";
 
 type UserType = {
@@ -10,13 +16,44 @@ type UserType = {
   email: string;
 };
 
-const SidebarUserButtonClient = ({ user }: UserType) => {
+const SidebarUserButtonClient = (user: UserType) => {
   const isMobile = useIsMobile();
 
   return (
     <SidebarMenu>
-      <DropdownMenu></DropdownMenu>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <SidebarMenuButton
+            size={"lg"}
+            className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground:"
+          >
+            <UserInfo {...user} />
+            <ChevronsUpDown className="ml-auto group-data-[state=collapsed]:hidden" />
+          </SidebarMenuButton>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>Hi</DropdownMenuContent>
+      </DropdownMenu>
     </SidebarMenu>
   );
 };
 export default SidebarUserButtonClient;
+
+const UserInfo = ({ name, imageUrl, email }: UserType) => {
+  const nameInitials = name
+    .split(" ")
+    .slice(0, 2)
+    .map((str) => str[0])
+    .join(" ");
+  return (
+    <Avatar className="rounded-lg size-8">
+      <AvatarImage src={imageUrl} alt={name} />
+      <AvatarFallback className="uppercase bg-primary text-primary-foreground">
+        {nameInitials}
+      </AvatarFallback>
+      <div className="flex flex-col flex-1 min-w-0 leading-tight group-data-[state-collapsed]:hidden">
+        <span className="truncate text-sm font-semibold">{name}</span>
+        <span className="truncate text-xs">{email}</span>
+      </div>
+    </Avatar>
+  );
+};
